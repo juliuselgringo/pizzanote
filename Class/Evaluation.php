b6de8cc168da241bcdc8a4d28b7d03180d0f7fc8
julius_el_gringo <julien.rousselot@hotmail.fr>
2025-06-24 17:53:20 +0200

    ?

--- Class/Evaluation.php
+++ Class/Evaluation.php
@@ -8,6 +8,32 @@ class Evaluation extends DBconnection{
         $evalQuery = "SELECT * FROM notation;";
         return $this->dbQuery($evalQuery);
     }
+
+    public function getSessionNote(){
+        $this->connectFct();
+        $evalQuery = "SELECT * FROM session;";
+        return $this->dbQuery($evalQuery);
+    }
+
+    public function sendEvaluation(){
+        $this->connectFct();
+        $idSession = $_SESSION['idSession'];
+        $name = $_SESSION['name'];
+        $restaurant = $_SESSION['restaurant'];
+        $alertMsg = '';
+        $dataNote = array_splice($_POST, 0, 27);
+        foreach($dataNote as $itemSousitem => $note){
+            $itemSousitemArr = explode("/", $itemSousitem);
+            $item = $itemSousitemArr[0];
+            $sousitem = $itemSousitemArr[1];
+            $resultQuery[] = "('" . $idSession . "','" . $restaurant . "','" . $item . "','" .$sousitem . "','" . $name . "'," . $note . ")";
+        }
+        $values = implode("," , $resultQuery);
+        $finalQuery = "INSERT INTO session (idSession, restaurant, item, sous_item, name, note) VALUES $values";
+        $alertMsg = $this->dbQuery($finalQuery, 'insert');
+        return $alertMsg;
+    }
+
 }
 
 ?>
--- check-notes.php
+++ check-notes.php
@@ -1,7 +1,17 @@
-<?php require_once "elements/head.php" ?>
+<?php 
+require_once 'Class/Evaluation.php';
+
+$check = new Evaluation;
+$datanote = $check->getSessionNote();
+
+require_once "elements/head.php";
+?>
 
 <body>
     <a href="index.php" >Accueil</a>
     <h1>Consultez les notes d'une session repas</h1>
-
+    <?php foreach($datanote as $dataLine): ?>
+        <p><?= $dataLine['idsession'] . ' ' . $dataLine['restaurant'] . ' ' . $dataLine['item'] . ' ' . $dataLine['sous_item'] . ' ' . $dataLine['name'] . ' ' . $dataLine['note'] ?></p>
+    <?php endforeach ?>
+    
 </body>
